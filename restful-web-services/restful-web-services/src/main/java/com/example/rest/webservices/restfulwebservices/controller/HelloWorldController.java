e27703b6cbd863ed7df31e44ed55ec0b04a6c946
Suchitra <suchitrasrinivas10@gmail.com>
2022-02-14 06:53:49 +0530

    Enhanced HelloworldController with Path variables

--- restful-web-services/restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ restful-web-services/restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.rest.webservices.restfulwebservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.rest.webservices.restfulwebservices.model.HelloWorldBean;
@@ -19,4 +20,9 @@ public class HelloWorldController {
 	}
 	
 
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
 }
