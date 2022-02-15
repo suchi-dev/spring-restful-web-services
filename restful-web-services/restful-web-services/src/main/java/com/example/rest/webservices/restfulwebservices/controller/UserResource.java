b3943a03c2268f6091c639732b3eb8240d64911d
Suchitra <suchitrasrinivas10@gmail.com>
2022-02-15 05:32:08 +0530

    Updated the postmapping to send the response status as Created and the location where the resource is created

--- restful-web-services/restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/controller/UserResource.java
+++ restful-web-services/restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/controller/UserResource.java
@@ -1,11 +1,16 @@
 package com.example.rest.webservices.restfulwebservices.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.rest.webservices.restfulwebservices.dao.UserDaoService;
 import com.example.rest.webservices.restfulwebservices.model.User;
@@ -15,17 +20,27 @@ public class UserResource {
 
 	@Autowired
 	private UserDaoService service;
-	
-	
+
 	@GetMapping("/users")
-	public List<User> retrieveAll(){
+	public List<User> retrieveAll() {
 		return service.findAll();
-		
+
 	}
-	
+
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
 	}
-	
+
+	@PostMapping("/users")
+	public ResponseEntity<Object> create(@RequestBody User user) {
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
+
+	}
+
 }
