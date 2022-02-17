27a77b64ba3476e5433de1f3be65656dc094e5af
Suchitra <suchitrasrinivas10@gmail.com>
2022-02-17 23:09:39 +0530

    Implemented Header, Request Param and Content-negotiation versioning

--- restful-web-services/restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ restful-web-services/restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -6,14 +6,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
+	/* URI Versioning */
 	@GetMapping("v1/person")
 	public PersonV1 personV1() {
 		return new PersonV1("Bob Charlie");
 	}
-	
+
 	@GetMapping("v2/person")
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
-	
+
+	/* Request Parameter versioning */
+
+	@GetMapping(value = "/person/param", params = "version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Bob Charlie");
+
+	}
+
+	@GetMapping(value = "/person/param", params = "version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+
+	/* Header versioning */
+
+	@GetMapping(value = "/person/headers", headers = "X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Bob Charlie");
+
+	}
+
+	@GetMapping(value = "/person/headers", headers = "X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+
+	/* Produces versioning also known as content-negotiation */
+
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+
+	}
+
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
 }
