531349ac81c80ff420a026591905300fe5c5b0de
Suchitra <suchitrasrinivas10@gmail.com>
2022-02-21 11:05:04 +0530

    Added Post entity and defined mapping between User and Post classes

--- restful-web-services/restful-web-services/src/main/resources/data.sql
+++ restful-web-services/restful-web-services/src/main/resources/data.sql
@@ -2,3 +2,8 @@ insert into user values(1001, SYSDATE(), 'AB');
 insert into user values(1002, SYSDATE(), 'Jill');
 insert into user values(1003, SYSDATE(), 'Jam');
 
+
+insert into post values(1101, 'This is my first post', 1001);
+insert into post values(1102, 'This is my second post', 1002);
+
+
